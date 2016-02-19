0eefca8db3c6f8407c437ca3ef8a286aac07650f
shaunvxc <shaun@yipitdata.com>
2016-02-19 02:11:38 -0500

    initial import

---
+++ envy/application.py
@@ -0,0 +1,115 @@
+#!/usr/bin/env python
+from __future__ import (absolute_import, division, print_function, unicode_literals)
+
+import sys
+import argparse
+import os
+import re
+
+from envy import VERSION
+
+VENV_ROOT = "~/.virtualenvs/{}/lib/python{}/site-packages/{}"
+ENVY_BASE = os.path.expanduser("~/.envies")
+
+def validate(f):
+    def wrapper(*args):
+        if not active_venv():
+            print("ERR: No active virtual env!")
+            return False
+
+        if not in_git_repo():
+            print("ERR: must be run from root of git repo")
+            return False
+
+        return f(args)
+
+    return wrapper
+
+def active_venv():
+    return '/.virtualenvs/' in sys.prefix
+
+def get_active_venv():
+    return re.search(".virtualenvs/([^/]{1,})/bin", sys.prefix).group(1)
+
+def in_git_repo():
+    path = os.getcwd()
+    return os.path.isdir(path + '/.git') or os.path.isdir(path + '/../.git')
+
+def get_package_name():
+    return os.getcwd().split("/")[-1]
+
+def get_envy_path():
+    return os.path.expanduser("~/.envies/{}".format(get_package_name()))
+
+# def get_py_version(venv):
+# return re.search("/.virtualenvs/{}/lib/python([2-3].[2-7])",
+
+def get_venv_package_path():
+    package_name = get_package_name()
+    venv = get_active_venv()
+
+    for py_version in ["2.7", "3.2", "3.3", "3.4"]:
+        if os.path.isdir(os.path.expanduser(VENV_ROOT.format(venv, py_version, package_name))):
+            return os.path.expanduser(VENV_ROOT.format(venv, py_version, package_name))
+
+
+    print("ERR: {}'s virtualenv does not contain {}".format(venv_name, project_name))
+    exit()
+
+
+def original_backed_up():
+    if not os.path.isdir(ENVY_BASE):
+        os.system('mkdir {}'.format(ENVY_BASE))
+
+    return os.path.isdir(get_envy_path())
+
+def back_up(venv_pkg_path):
+    import pdb;pdb.set_trace()
+    os.system("cp -r {} {}".format(venv_pkg_path, get_envy_path()))
+
+@validate
+def sink_all(*args):
+    venv_pkg_path = get_venv_package_path()
+
+    if not original_backed_up():
+        back_up(venv_pkg_path)
+
+    os.system("cp *.py {}".format(venv_pkg_path))
+    print ("Sinked")
+
+@validate
+def sink_clean(*args):
+    venv_pkg_path = get_venv_package_path()
+    # remove applied changes
+    os.system("rm -rf {}".format(venv_pkg_path))
+    # copy over the original package from ~/.envies
+    os.system("cp -r {} {}".format(get_envy_path(), venv_pkg_path))
+    # remove envy backup
+    os.system("rm -rf {}".format(get_envy_path()))
+
+
+def prepare_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--version', action='version', version='%(prog)s ' + VERSION)
+
+    subparsers = parser.add_subparsers(dest='command_name')
+
+    parser_search = subparsers.add_parser('all', help='sync all files to active virtualenv')
+
+    parser_search.set_defaults(func=sink_all)
+    parser_search.add_argument('filename', nargs='*')
+
+    parser_list = subparsers.add_parser('clean', help='reset virtualenv to original state')
+    parser_list.set_defaults(func=sink_clean)
+
+    return parser
+
+
+def main():
+    parser = prepare_parser()
+    args = parser.parse_args()
+
+    if hasattr(args, 'func'):
+        args.func(args)
+    else:
+        parser.print_help()
---
+++ setup.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+
+import os
+
+from setuptools import setup
+
+from envy import VERSION
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+# Get the long description from the README file
+# with open(os.path.join(here, 'README.rst'), 'rU') as f:
+#     long_description = f.read()
+
+required = [
+    'future'
+]
+
+setup(
+    name='envy',
+    version=VERSION,
+    packages=['envy'],
+    url='https://github.com/shaunvxc/envy',
+    download_url='https://github.com/shaunvxc/envy/tarball/' + VERSION,
+    license='MIT',
+    author='Shaun Viguerie',
+    author_email='shaunvig114@gmail.com',
+    description='sync and unsync local changes to virtualenvs',
+    entry_points={
+        'console_scripts': ['envy = envy.application:main']
+    },
+    install_requires=required,
+    # extras_require=extras,
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 3 - Alpha',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'Intended Audience :: End Users/Desktop',
+        'Topic :: System :: Shells',
+        'Topic :: System :: System Shells',
+
+        # Pick your license as you wish (should match "license" above)
+        'License :: OSI Approved :: MIT License',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ]
+)
